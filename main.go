346152aaa3c18ac13b08a859414901bd17783a01
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-10 11:08:09 +0400

    Discover variables, strings and numbers
    - different ways to init variables
    - ints and floats

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
-// this code should be compile to an executable code at the end
-// so if we write some utils functions, it will something else than main
 package main
 
 import "fmt"
 
-// this function called main is important because it is the entry point of our app
-// we should have juste one function with this name in our project
 func main() {
+	// here we specify explicitly the type of the variable
 	var name string = "Pedro"
-	fmt.Println("Hello", name)
+	// but GO can do it by its own
+	var nameTwo = "John"
+
+	// the fast way to init a var is like so, but we can use this method only in function
+	nameThree := "Guigui"
+
+	fmt.Println(name, nameTwo, nameThree)
+
+	// like Python, GO has int and float numbers
+
+	// int number
+	intNumber := 15
+
+	// float number
+	floatNumber := 10.2
+
+	fmt.Println(intNumber, floatNumber)
 }
