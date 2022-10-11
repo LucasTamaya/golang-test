efe62d622399ed0275230780c04567f2184f4e49
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-11 14:48:35 +0400

    Pointer
    - access to memory address
    - access to value of a memory address

--- main.go
+++ main.go
@@ -1,33 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateNameByMemoryAddress(v *string) {
+	*v = "wedge"
 }
 
 func main() {
-	// group A types -> strings, ints, bools, floats, arrays, structs
-	// non-pointer wrapper values
-	name := "tifa"
+	name := "John"
 
-	// updateName(name)
-	name = updateName(name)
+	// $var allow us to access to the memory address of variables
+	fmt.Println("The memory address of name is", &name)
+
+	nameMemoryValue := &name
+	// *&name allow us to acces to the value of a memory address
+
+	updateNameByMemoryAddress(nameMemoryValue)
 
 	fmt.Println(name)
-
-	// group B types -> slices, maps, functions
-	// pointer wrapper values
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 }
