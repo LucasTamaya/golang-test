ef45b37da79f7164b415153d839b2e1e1ff2c3bd
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-11 13:23:35 +0400

    Package scope
    - create file and import function

--- main.go
+++ main.go
@@ -1,39 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func sayHello(name string) {
-	fmt.Printf("Hello %v", name)
-}
-
-// if we want to return, we must specify the return type value, like TS
-func sum(a int, b int) int {
-	return a + b
-}
-
-// return mutliples elements in a function
-func getInitials(n string) (string, string) {
-
-	// we use the strings lib to split the names
-	splitNames := strings.Split(n, " ")
-
-	var initials []string
-
-	for _, v := range splitNames {
-		initials = append(initials, v[:1])
-	}
-
-	return initials[0], initials[1]
-
-}
+import "fmt"
 
 func main() {
-	sayHello("John Doe")
-
-	first, second := getInitials("John Doe")
-
-	fmt.Println(first, second)
+	// we access to the sum function from the sum.go file
+	// but we have to run those two files: run go main.go sum.go
+	theSum := sum(10, 20)
+	fmt.Println(theSum)
 }
---
+++ sum.go
@@ -0,0 +1,7 @@
+// because we type package main, all variables and functions of this file will be accessible from all
+// others files that are inside the package main
+package main
+
+func sum(a int, b int) int {
+	return a + b
+}
