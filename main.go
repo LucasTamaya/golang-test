971d3035503942cd6ad340b3f24cde6bfb7c33d3
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-11 12:51:30 +0400

    Discover functions
    - just like TS

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func sayHello(name string) {
+	fmt.Printf("Hello %v", name)
+}
+
+// if we want to return, we must specify the return type value, like TS
+func sum(a int, b int) int {
+	return a + b
+}
 
 func main() {
-
-	compteur := 0
-
-	for compteur <= 10 {
-		fmt.Println(compteur)
-
-		if compteur == 10 {
-			fmt.Println("BOOM")
-			compteur++
-			break
-		}
-
-		compteur++
-	}
-
-	names := []string{"Jojo", "Lolo", "Koko"}
-	ages := []int{10, 15, 20}
-
-	// loop throught an array with index and value
-	for index, value := range names {
-		fmt.Printf("The index is %v, and the value is %v \n", index, value)
-	}
-
-	// loop just throught value
-	for _, value := range ages {
-		fmt.Printf("The age is %v \n", value)
-	}
-
+	sayHello("John Doe")
 }
