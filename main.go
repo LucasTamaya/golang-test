200ee9cb7d237ab6a4c8a56d02fc37bb885b5d38
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-11 20:39:42 +0400

    Add a new todo
    -new post route with new function

--- main.go
+++ main.go
@@ -23,12 +23,28 @@ func getTodos(ctx *gin.Context) {
 	ctx.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(ctx *gin.Context) {
+	// init an empty variable of type todo
+	var newTodo todo
+
+	// ctx.BindJSON get the body of the request and append it to the memory address of newTodo
+	if err := ctx.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	ctx.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	// create the router
 	router := gin.Default()
 
 	// create some routes
-	router.GET("/", getTodos)
+	router.GET("/todos", getTodos)
+	router.GET("/todos")
+	router.POST("/todos", addTodo)
 
 	// run the app at localhost port 8080
 	router.Run("localhost:8080")
